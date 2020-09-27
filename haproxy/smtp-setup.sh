2eb7d6cf0a9bb8f62dd363bed1f5dbd8bc6b6504
root <root@rpatilvm1.uomyqqse4u4ubouwxxbhulpsff.tx.internal.cloudapp.net>
2020-09-27 12:40:49 +0000

    type in smtp.sh

--- haproxy/smtp-setup.sh
+++ haproxy/smtp-setup.sh
@@ -2,6 +2,7 @@
 
 vmname=`hostname -f`
 apt-get -y update
+sudo add-apt-repository universe
 debconf-set-selections <<< "postfix postfix/mailname string $vname"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install -y mailutils
